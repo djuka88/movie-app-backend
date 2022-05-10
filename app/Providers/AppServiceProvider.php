3c29d3ffdb157a15cd5ba80506fcb842f32e0214
Milan <milan.djukic@vivifyideas.com>
2022-05-10 07:53:08 +0200

    Added jwt support and added register function

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,9 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\DB;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -23,6 +26,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        DB::listen(function ($query) {
+            // $query->sql;
+            // $query->bindings;
+            // $query->time;
+            Log::info($query->sql);
+        });
     }
 }
